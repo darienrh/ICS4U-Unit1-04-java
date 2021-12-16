2864d98df7b6344bb1a82917cbde2cde2bb91bc6
darienrh <94391400+darienrh@users.noreply.github.com>
2021-12-16 17:52:33 +0000

    all

--- Main.java
+++ Main.java
@@ -52,10 +52,10 @@ final class Main {
             }
             if (myGuess == computerPick) {
                 System.out.println();
-                System.out.println("You WIN! The computer picked " +
-                   computerPick);
-                System.out.println("You took " + trys +
-                   " trys to guess the correct number");
+                System.out.println("You WIN! The computer picked " 
+                  + computerPick);
+                System.out.println("You took " + trys 
+                  + " trys to guess the correct number");
                 win = true;
             }
             if (myGuess > 6 || myGuess < 1) {   
